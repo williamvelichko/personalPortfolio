bc43d86aeaead94709b4546d5d65234b914638fd
williamvelichko <williamvelichko2003@gmail.com>
2023-01-12 12:59:42 -0800

    started on work

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,8 +3,46 @@ import "../Styling/Work.css";
 function Work() {
   return (
     <div className="container">
-      <div className="work_container">Where i work</div>
-      <div className="projects_container">Some things i built</div>
+      <div className="work_container">
+        <h3>Where I worked</h3>
+        <div className="company">
+          <h5>Busy Buccket:</h5>
+          <ul>
+            <li>
+              <p>
+                Lorem Ipsum es simplemente el texto de relleno de las imprentas
+                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
+                estándar de las industrias desde el año 1500, cuando un impresor
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem Ipsum es simplemente el texto de relleno de las imprentas
+                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
+                estándar de las industrias desde el año 1500, cuando un impresor
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem Ipsum es simplemente el texto de relleno de las imprentas
+                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
+                estándar de las industrias desde el año 1500, cuando un impresor
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem Ipsum es simplemente el texto de relleno de las imprentas
+                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
+                estándar de las industrias desde el año 1500, cuando un impresor
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="projects_container">
+        <h3>Some things i built</h3>
+        <div className="projects"></div>
+      </div>
     </div>
   );
 }
