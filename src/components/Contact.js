af67f869235a96aad7c783759bdce9f473947ba3
williamvelichko <williamvelichko2003@gmail.com>
2023-01-16 12:35:28 -0800

    save

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
+  const [userInfo, SetUserInfo] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
   return (
-    <div>
+    <div className="container">
       <h2>Contact</h2>
+      <div className="form_container">
+        <form>
+          <input
+            type="name"
+            placeholder="name"
+            name="name"
+            value={userInfo.name}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={userInfo.email}
+          />
+          <textarea
+            type="message"
+            placeholder="message"
+            name="message"
+            value={userInfo.message}
+          ></textarea>
+        </form>
+      </div>
     </div>
   );
 }
