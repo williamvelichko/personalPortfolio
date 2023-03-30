098607c8864d1facdf4e28d9ea3bd38efbe0a810
williamvelichko <williamvelichko2003@gmail.com>
2023-03-29 20:34:01 -0700

    update

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,46 +1,53 @@
 import React from "react";
 import "../Styling/Work.css";
+import img from "../Images/ScreenShot.png";
 function Work() {
   return (
     <div className="container">
       <div className="work_container">
-        <h3>Where I worked</h3>
+        <div className="container_title">
+          <h3>Where I worked</h3>
+        </div>
         <div className="company">
-          <h5>Busy Buccket:</h5>
+          <h3>Busy Buccket:</h3>
           <div className="Iframe_BusyBucket">
             <iframe
               src="https://www.busybucket.io/"
-              width={800}
-              height={500}
+              width={"800px"}
+              height={"500px"}
             ></iframe>
           </div>
           <ul>
             <li>
               <p>
-                Lorem Ipsum es simplemente el texto de relleno de las imprentas
-                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
-                estándar de las industrias desde el año 1500, cuando un impresor
+                Developed and maintained the application using technologies such
+                as HTML, CSS, and TypeScript
               </p>
             </li>
             <li>
               <p>
-                Lorem Ipsum es simplemente el texto de relleno de las imprentas
-                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
-                estándar de las industrias desde el año 1500, cuando un impresor
+                Built reusable and maintainable Frontend code using modern
+                frameworks and libraries such as ReactJS
               </p>
             </li>
             <li>
               <p>
-                Lorem Ipsum es simplemente el texto de relleno de las imprentas
-                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
-                estándar de las industrias desde el año 1500, cuando un impresor
+                Discovered and reported critical issues in the application,
+                leading to improved functionality and customer satisfaction{" "}
               </p>
             </li>
             <li>
               <p>
-                Lorem Ipsum es simplemente el texto de relleno de las imprentas
-                y archivos de texto. Lorem Ipsum ha sido el texto de relleno
-                estándar de las industrias desde el año 1500, cuando un impresor
+                Developed and implemented solutions for frontend issues,
+                resulting in improved user experience and increased website
+                performance
+              </p>
+            </li>
+            <li>
+              <p>
+                Participated in code reviews and Agile development processes,
+                contributing to the development of high-quality and maintainable
+                code
               </p>
             </li>
           </ul>
@@ -48,47 +55,52 @@ function Work() {
       </div>
       <div className="projects_container">
         <div className="projects">
-          <h3>Some things i built</h3>
+          <div className="container_title">
+            <h3>Some things i built</h3>
+          </div>
           <div className="singleProject">
-            <h5>MojoPlanner:</h5>
-            <div className="Iframe_MojoPlanner">
+            <h3>MojoPlanner:</h3>
+            <img src={require("../Images/ScreenShot.png")} />
+            {/* <div className="Iframe_MojoPlanner">
               <iframe
                 src="https://www.mojoplanning.com/"
                 width={800}
                 height={500}
               ></iframe>
-            </div>
+            </div> */}
             <ul>
               <li>
                 <p>
-                  Lorem Ipsum es simplemente el texto de relleno de las
-                  imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
-                  relleno estándar de las industrias desde el año 1500, cuando
-                  un impresor
+                  Developed full stack web application using Javascript,
+                  ReactJS, Node.js that allows user to sign-up and login, allows
+                  user to create, edit, delete projects, and users are able to
+                  assign, update, complete tasks for each project created{" "}
+                </p>
+              </li>
+
+              <li>
+                <p>
+                  Created and implemented Figma design on the Frontend using
+                  CSS, Styled-components, and Html for the best UI/UX
                 </p>
               </li>
               <li>
                 <p>
-                  Lorem Ipsum es simplemente el texto de relleno de las
-                  imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
-                  relleno estándar de las industrias desde el año 1500, cuando
-                  un impresor
+                  Generated data tables using Knex.js and PostgreSQL for the
+                  ability to create and store users, projects, and tasks
                 </p>
               </li>
               <li>
                 <p>
-                  Lorem Ipsum es simplemente el texto de relleno de las
-                  imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
-                  relleno estándar de las industrias desde el año 1500, cuando
-                  un impresor
+                  Constructed user authentication and user authorization with
+                  the use of a JWT token and API endpoints that gives users the
+                  ability to sign-up and login
                 </p>
               </li>
               <li>
                 <p>
-                  Lorem Ipsum es simplemente el texto de relleno de las
-                  imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
-                  relleno estándar de las industrias desde el año 1500, cuando
-                  un impresor
+                  Implemented email confirmation with nodemailer which confirms
+                  a user is using a real email
                 </p>
               </li>
             </ul>
