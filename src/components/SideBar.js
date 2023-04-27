9df82ff653c020078c88faf2d954990b6861aece
williamvelichko <williamvelichko2003@gmail.com>
2023-04-27 15:07:16 -0700

    save

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,31 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Styling/SideBar.css";
-
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
 function SideBar() {
   return (
     <div className="sideBar_container">
       <div classname="sideBar_logo">
         <Link to="/landingpage" className="logo_link">
-          <h3>WV</h3>
+          <span className="letter-w">W</span>
+          <span className="letter-v">V</span>
         </Link>
         <h5>William Velichko</h5>
       </div>
       <div className="sideBar_nav">
         <Link className="link" to="/about ">
-          About
+          <p>About</p>
         </Link>
         <Link className="link" to="/skills">
-          Skills
+          <p>Skills</p>
         </Link>
         <Link className="link" to="/work">
-          Work
+          <p> Work</p>
         </Link>
         <Link className="link" to="/contact">
-          Contact
+          <p>Contact</p>
         </Link>
       </div>
-      <div className="sideBar_media">Media</div>
+      <div className="sideBar_media">
+        <p>
+          <TwitterIcon />
+        </p>
+        <p>
+          <InstagramIcon />
+        </p>
+        <p>
+          <LinkedInIcon />
+        </p>
+        <p>
+          <GitHubIcon />
+        </p>
+      </div>
     </div>
   );
 }
