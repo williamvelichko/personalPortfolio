775d007ca929027f6659dcb74ec2dcb3ecd15f39
williamvelichko <williamvelichko2003@gmail.com>
2023-04-27 18:51:36 -0700

    scroll

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,39 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Styling/LandingPage.css";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 function LandingPage() {
   return (
     <div className="landing_container">
-      <div className="landing_title">
-        <div className="hello_container">
-          <h2>Hello 👋 I'm</h2>
-          <section className="landing_animation">
-            <div className="first">
-              <div>William Velichko</div>
-            </div>
-            <div className="second">
-              <div>Software Engineer</div>
-            </div>
-            <div className="third">
-              <div>FullStack Developer</div>
-            </div>
-          </section>
+      <div className="landing_min">
+        <div className="landing_title">
+          <div className="hello_container">
+            <h2>Hello 👋 I'm</h2>
+            <section className="landing_animation">
+              <div className="first">
+                <div>William Velichko</div>
+              </div>
+              <div className="second">
+                <div>Software Engineer</div>
+              </div>
+              <div className="third">
+                <div>FullStack Developer</div>
+              </div>
+            </section>
+          </div>
+          <p>
+            <em>Get To Know Me</em>
+          </p>
         </div>
+
+        <div className="landing_contact_button">
+          <Link className="landing_contact" to="/contact">
+            Contact
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </Link>
+        </div>
+      </div>
+      <div className="landing_scroll">
         <p>
-          <em>Get To Know Me</em>
+          <ArrowDownwardIcon fontSize="large" />
+        </p>
+        <p>
+          <ArrowDownwardIcon fontSize="large" />
         </p>
       </div>
-
-      <div className="landing_contact_button">
-        <Link className="landing_contact" to="/contact">
-          Contact
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </Link>
-      </div>
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
 }
